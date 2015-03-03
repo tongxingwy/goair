ae20ea1cf0a1fc4f24218f3a358bbec54abce91d
tongxingwy <tongxingwy@gmail.com>
2015-03-03 10:45:07 +0800

    Update main.go

--- example/main.go
+++ example/main.go
@@ -11,13 +11,13 @@ type airplayHandler struct {
 // Get this party started.
 func main() {
 	h := airplayHandler{}
-	nighthawk.Start("nighthawk", &h) // set the display name of your server.
+	goair.Start("goair", &h) // set the display name of your server.
 }
 
-func (h *airplayHandler) ReceivedAudioPacket(c *nighthawk.Client, data []byte, length int) {
+func (h *airplayHandler) ReceivedAudioPacket(c *goair.Client, data []byte, length int) {
 	//log.Println("got an audio packet")
 }
 
-func (h *airplayHandler) SupportedMirrorFeatures() nighthawk.MirrorFeatures {
-	return nighthawk.MirrorFeatures{Height: 1280, Width: 720, Overscanned: true, RefreshRate: 0.016666666666666666}
+func (h *airplayHandler) SupportedMirrorFeatures() goair.MirrorFeatures {
+	return goair.MirrorFeatures{Height: 1280, Width: 720, Overscanned: true, RefreshRate: 0.016666666666666666}
 }
